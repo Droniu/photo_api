1d6e510a8e62ec3b77e3dd6da9772b3c0f6a3c38
Droniu <dron.official@yahoo.com>
2021-07-11 22:00:30 +0200

    catalog urls

--- gallery/urls.py
+++ gallery/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("photos/", views.PhotoList.as_view()),
-    path("photos/<int:id>", views.PhotoDetail.as_view())
+    path("photos/<int:id>", views.PhotoDetail.as_view()),
+    path("catalogs/", views.CatalogList.as_view()),
+    path("catalogs/<int:id>", views.CatalogDetail.as_view()),
 ]
