8d340a4e2b87e0cd6903d8bf378ceb906c1eb770
Droniu <dron.official@yahoo.com>
2021-07-12 16:10:26 +0200

    static media roots

--- photo_api/settings.py
+++ photo_api/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import environ
+import os
 import rest_framework
 
 # Initialize environment variables
@@ -162,10 +163,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '~/photo_api/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
