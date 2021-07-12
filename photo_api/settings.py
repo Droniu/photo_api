00404dc45d3aa181df4e10543c557b33c443cc8f
Droniu <dron.official@yahoo.com>
2021-07-12 17:51:09 +0200

    allowed hosts fix

--- photo_api/settings.py
+++ photo_api/settings.py
@@ -17,7 +17,9 @@ import os
 import rest_framework
 
 # Initialize environment variables
-env = environ.Env()
+env = environ.Env(
+    DEBUG=(bool, False)
+)
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,10 +33,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env('DEBUG')
+DEBUG = env('DEBUG', False)
 
 ALLOWED_HOSTS = [
     "api.droniu.pl"
+    "www.api.droniu.pl"
     "localhost"
 ]
 
