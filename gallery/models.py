123c225536973cc470237cfac771b84598d8ec03
Droniu <dron.official@yahoo.com>
2021-07-12 19:21:15 +0200

    another fix for migrations

--- gallery/models.py
+++ gallery/models.py
@@ -9,13 +9,13 @@ def cover_path(instance, filename):
 
 # Create your models here.
 class Catalog(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     cover = models.ImageField(upload_to=cover_path)
     title = models.CharField(max_length=255)
     subtitle = models.CharField(max_length=255, blank=True)
 
 
 class Photo(models.Model):
-    uuid = models.UUIDField(default=uuid.uuid4, editable=False)
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     catalog = models.ForeignKey(Catalog, on_delete=models.CASCADE, null=True)
     img = models.ImageField(upload_to=photo_path, null=True)
